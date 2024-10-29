c9cec83928f01672f241cc4085924999e53834c5
vaibhav <vaibhavnagargoje2000@gmail.com>
2024-10-29 22:06:01 +0530

    testing data with request module

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,6 @@ from api import views
 from django.urls import path
 
 urlpatterns = [
-    path('/<int:key>',views.student_detail,name="student"),
-    path('/list',views.student_list,name ="student_list")
+    path('<int:key>/',views.student_detail,name="student"),
+    path('list/',views.student_list,name ="student_list")
 ]
